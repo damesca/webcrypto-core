341f331cfe4664ce4cc4d23649f2355e9b7a9f5f
Daniel Morales <damesca@lcc.uma.es>
2020-05-26 12:36:11 +0200

    Organization of files

--- src/ot/base.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const provider_1 = require("../provider");
-class OtProvider extends provider_1.ProviderCrypto {
-}
-exports.OtProvider = OtProvider;
